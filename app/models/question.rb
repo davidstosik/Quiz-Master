8d5d5bb072c8a7655e5702f12de11c1f319fcff9
David Stosik <david.stosik+git-noreply@gmail.com>
2016-12-02 09:52:05 +0900

    Implement simple Question#answer_is_correct?
    Test is based on string equality (case insensitive)

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,9 @@
 class Question < ApplicationRecord
   validates_presence_of :body
   validates_presence_of :answer
+
+  def answer_is_correct?(answer)
+    return true if answer.downcase == self.answer.downcase
+    # TODO number to words
+  end
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Question, type: :model do
   let(:question) { build(:question) }
+
   describe 'attributes' do
     it 'should have a body' do
       expect(question.body).to be_present
@@ -14,5 +15,34 @@ RSpec.describe Question, type: :model do
   describe 'validations' do
     it { is_expected.to validate_presence_of(:body) }
     it { is_expected.to validate_presence_of(:answer) }
+  end
+
+  describe '#answer_is_correct?' do
+    subject { question.answer_is_correct?(answer) }
+    context 'when answer is correct' do
+      let(:answer) { question.answer }
+      it { is_expected.to be_truthy }
+    end
+    context 'when answer is not correct' do
+      let(:answer) { 'incorrect' }
+      it { is_expected.to be_falsy }
+    end
+    context 'when the answer is correct with a different letter case' do
+      let(:question) { build(:question, answer: 'lower case') }
+      let(:answer) { 'Lower Case' }
+      it { is_expected.to be_truthy }
+    end
+    context 'when the correct answer is numeric' do
+      before { skip('TODO: number to words') }
+      let(:question) { build(:question, answer: '5') }
+      context 'and passed answer is the same number' do
+        let(:answer) { 5 }
+        it { is_expected.to be_truthy }
+      end
+      context 'and passed answer is the corresponding number in words' do
+        let(:answer) { 'five' }
+        it { is_expected.to be_truthy }
+      end
+    end
   end
 end

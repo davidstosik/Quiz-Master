0a4b2911a5c873e0e726482a7b911bb1011fa448
David Stosik <david.stosik+git-noreply@gmail.com>
2016-12-04 19:56:51 +0900

    Rescue all exceptions on JSON format, and return JSON-formatted errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception, unless: -> { request.format.json? }
+  rescue_from Exception, with: :rescue_exception_with_json
+
+  private
+
+  def rescue_exception_with_json(exception)
+    raise unless request.format.json?
+    status = ActionDispatch::ExceptionWrapper.status_code_for_exception(exception.class.name)
+    render json: { error: exception.message }, status: status
+  end
+
 end
